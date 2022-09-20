29b5739e7a9bdb4c5a99a19704ae44f887ce14df
Sumair10 <sumairzahid123@gmail.com>
2022-09-20 13:13:05 +0500

    new

--- sw.js
+++ sw.js
@@ -14,18 +14,18 @@ const assets = [
 
 // install event
 self.addEventListener('install', evt => {
-  //console.log('service worker installed');
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  );
+  console.log('service worker installed');
+  // evt.waitUntil(
+  //   caches.open(staticCacheName).then((cache) => {
+  //     console.log('caching shell assets');
+  //     cache.addAll(assets);
+  //   })
+  // );
 });
-
 // activate event
+
 self.addEventListener('activate', evt => {
-  //console.log('service worker activated');
+  console.log('service worker activated');
 });
 
 // fetch event
